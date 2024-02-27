dbb944fc5ee64e8bb90d8815dda4656c7e8b5240
Anki <meriidmerihihe@gmail.com>
2024-02-27 18:39:18 +0530

    name problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Header from "./Header";
 import ProductList from "./ProductList";
-import { items } from "./items";
-import Footer from "./footer";
+import { items } from "./Items";
+import Footer from "./Footer";
 
 function App() {
   return (
