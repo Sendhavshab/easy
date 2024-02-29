d5bb6845291c8ef81c37fb7fd5ec86c8c7fb3fa9
AnkiTools <152778132+Sendhavshab@users.noreply.github.com>
2024-02-29 18:45:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import ProductList from "./ProductList";
-import { items } from "./Items";
+import { items } from "./Data";
 import Footer from "./Footer";
 
 function App() {
